42ada4b8214bd80b9362074166680a55f5770d49
JacopoNa <j.nardelli@virgilio.it>
2022-07-06 15:11:35 +0200

    header done

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="dc logo">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li class="current">COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        height: 100px;
+        font-size: 10px;
+        font-weight: 600;
+
+        .logo {
+            img {
+                width: 70px;
+            }
+        }
+
+        .menu {
+
+            ul {
+                li{
+                    margin: 0 13px;
+                    line-height: 100px;
+                    
+                    &.current {
+                        color: blue;
+                        border-bottom: 3px solid blue;
+                    }
+                }
+            }
+        }
+    }
+</style>
