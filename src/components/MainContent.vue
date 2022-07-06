a6ccb9cad40eb3c6c269afeb5dfe67db7f930934
JacopoNa <j.nardelli@virgilio.it>
2022-07-06 15:32:13 +0200

    section MainContent

---
+++ src/components/MainContent.vue
@@ -0,0 +1,22 @@
+<template>
+        <section class="content">
+            <div class="container">
+                <span> --> Content goes here </span>
+            </div>
+        </section>
+</template>
+
+<script>
+export default {
+    name: 'MainContent'
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .content {
+        background-color: #1c1c1c;
+        color: white;
+        padding: 40px 0;
+    }
+</style>
